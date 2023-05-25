67d540b0ff842d5c8c9f6a2b9cb7b67d8d593e09
JoaoUFG <joao.tavares@discente.ufg.br>
2023-05-24 23:11:09 -0300

    Deploy do projeto SPRING

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Portifólio
+## Projeto SPRING
+- Link: http://ec2-3-85-171-33.compute-1.amazonaws.com:8080/
+- Trabalho em grupo 
+- O projeto spring foi construído para consumir cadastros de outro banco, por isso nele há a ausência de formulários de cadastro. 
+- Os CPFs 123, 456, 789 e 999 são cadastros padrão.
+- A senha para cada um deles é o próprio CPF, cada conta possui seus acessos, sendo que o 999 é o administrador.
+  
 * É o repositório onde guardo alguns de meus projetos da universidade, projetos pessoais e de treinamento.
 * Fique a vontade para navegar pelas pastas e ver o que eu ando aprendendo.
---
+++ [SPRING] Sistema de Elaboração de Provas/sep/src/main/java/br/ufg/sep/views/login/LoginView.java
@@ -0,0 +1,51 @@
+package br.ufg.sep.views.login;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import br.ufg.sep.data.repositories.CadastroRepository;
+
+@Route("/login")
+@PageTitle("Login")
+public class LoginView extends VerticalLayout implements BeforeEnterObserver{
+
+	LoginForm login = new LoginForm();
+	
+	public LoginView(CadastroRepository cadastroRepository) {
+		
+		
+		setSizeFull();
+		setJustifyContentMode(JustifyContentMode.CENTER);
+		setAlignItems(Alignment.CENTER);
+		login.setAction("login");
+		
+		add(
+				new H1("SEP LOGIN"),
+				login
+				);
+		
+	}
+
+	@Override
+	public void beforeEnter(BeforeEnterEvent event) {
+
+		if(event.getLocation()
+				.getQueryParameters()
+				.getParameters()
+				.containsKey("error")) {
+			login.setError(true);
+		}
+				
+		
+	}
+	
+	
+	
+	
+	
+}
